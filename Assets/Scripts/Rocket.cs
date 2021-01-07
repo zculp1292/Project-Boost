e410a7279cdf6d315ba2b8a8b9bac4219d3c2aa9
Zachary Culpepper <zculp4579@gmail.com>
2021-01-07 12:16:08 -0600

    Added Movement to the Rocket. Basic Thrust on the Spacebar, Rotation on the A/D keys.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody rocketRigidBody;
+    
     void Start()
     {
-        
+        rocketRigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        print("Update");
+        MovementInput();
+    }
+
+    private void MovementInput()
+    {
+        if(Input.GetKey(KeyCode.Space)) //can thrust while rotating
+        {
+            rocketRigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A)) //can only rotate in one direction at a time
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
     }
 }
