817c81f30757584d4b68b4fcf7c678177712e4ce
Zachary Culpepper <zculp4579@gmail.com>
2021-01-07 21:44:17 -0600

    Refactored Rocket.cs. Fixed movement bugs and added testing platforms.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -16,24 +16,14 @@ public class Rocket : MonoBehaviour
 
     void Update()
     {
-        MovementInput();
+        Thrust();
+        Rotate();
     }
 
-    private void MovementInput()
+    private void Rotate()
     {
-        if(Input.GetKey(KeyCode.Space)) //can thrust while rotating
-        {
-            rocketRigidBody.AddRelativeForce(Vector3.up);
-            if (thrustSound.isPlaying == false) //prevents audio from playing on top of itself
-            {
-                thrustSound.Play();
-            }
-        }
-        else
-        {
-            thrustSound.Stop();
-        }
-
+        rocketRigidBody.freezeRotation = true; //takes control of physics
+        
         if (Input.GetKey(KeyCode.A)) //can only rotate in one direction at a time
         {
             transform.Rotate(Vector3.forward);
@@ -41,6 +31,24 @@ public class Rocket : MonoBehaviour
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward);
+        }
+
+        rocketRigidBody.freezeRotation = false; //release control of physics
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
+        {
+            rocketRigidBody.AddRelativeForce(Vector3.up);
+            if (thrustSound.isPlaying == false) //prevents audio from playing on top of itself
+            {
+                thrustSound.Play();
+            }
+        }
+        else //stops audio when Thrust key is released
+        {
+            thrustSound.Stop();
         }
     }
 }
